91cc317a8ba509b09a0aef5c7e4152613617d4a1
J Monsen <jmonsen@Js-MacBook-Pro.local>
2021-04-03 13:50:24 -0600

    Week 1 Submission

---
+++ index.js
@@ -0,0 +1,23 @@
+var itemPrice = 2.20;
+var amountOfMoneyInWallet = 40.00;
+var numberOfFriends = 9;
+var ageInYears = 25;
+var firstName = 'J';
+var lastName = 'Monsen';
+var middleInitial = 'S';
+
+var newAmountOfMoney = amountOfMoneyInWallet - itemPrice;
+var yearlyFriends = ageInYears + numberOfFriends;
+var fullName = firstName + " " + middleInitial +  " " + lastName;
+
+
+console.log(itemPrice);
+console.log(amountOfMoneyInWallet);
+console.log(numberOfFriends);
+console.log(numberOfFriends);
+console.log(ageInYears);
+console.log(firstName);
+console.log(middleInitial);
+console.log(newAmountOfMoney);
+console.log(yearlyFriends);
+console.log(fullName);
